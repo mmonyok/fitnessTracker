e36775d192d2c573c31e0d883b22469fcc20a2b8
Ody <melodymonyok@gmail.com>
2021-08-22 20:35:34 -0500

    Updated the routes; i am now using a controllers folder for better organization, and that is also what I am use to.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 // const logger = require("morgan");
+const path = require("path");
 const mongoose = require("mongoose");
+const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,19 +12,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("public"));
+app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
   useNewUrlParser: true,
-  // useUnifiedTopology: true,
+  useUnifiedTopology: true,
   // useCreateIndex: true,
   useFindAndModify: false
 });
-
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
