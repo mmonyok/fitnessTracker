8788b6678f50fb72c0fb65a61996ceec358c680a
Ody <melodymonyok@gmail.com>
2021-08-24 18:34:46 -0500

    another test

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
-const db = require("../../models")
+const { Workout } = require("../../models");
+// const db = require("../../models")
 
 // This route gets the data for the "last workout display."
 router.get("/workouts", async (req, res) => {
   try {
     // Here we get all workouts and then sort from most recent and only take one; then we add the total duration field.
-    const data = await db.Workout.aggregate([
+    const data = await Workout.aggregate([
       { $sort: { _id: -1 } },
       { $limit: 1 },
       {
@@ -22,7 +23,7 @@ router.get("/workouts", async (req, res) => {
 // This route creates an instance of Workout.
 router.post("/workouts", async (req, res) => {
   try {
-    const data = await db.Workout.create(req);
+    const data = await Workout.create(req);
     res.json(data);
   } catch (err) {
     console.log(err);
@@ -34,7 +35,7 @@ router.post("/workouts", async (req, res) => {
 router.put("/workouts/:id", async ({ body, params }, res) => {
   try {
     // Here we select the correct document and add our exercise to it.
-    const data = await db.Workout.updateOne({
+    const data = await Workout.updateOne({
       "_id": params.id
     }, {
       $addToSet: { "exercises": [body] }
@@ -50,7 +51,7 @@ router.put("/workouts/:id", async ({ body, params }, res) => {
 router.get("/workouts/range", async (req, res) => {
   try {
     // Here we get all workouts and sort by most recent, add a total duration field, and take the most recent 7.
-    const data = await db.Workout.aggregate([
+    const data = await Workout.aggregate([
       { $sort: { _id: -1 } },
       { $limit: 7 },
       { $sort: { _id: 1 } },
