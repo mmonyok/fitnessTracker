47a9d0d209da3c2bac1b85e9768b2aea326e2363
Ody <melodymonyok@gmail.com>
2021-08-23 22:50:08 -0500

    Updated the get route for the last workout to now show total duration and all properties of the workout.

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -2,27 +2,36 @@ const router = require("express").Router();
 const { Workout } = require("../../models");
 const db = require("../../models")
 
+// This route gets the data for the "last workout display."
 router.get("/workouts", async (req, res) => {
   try {
     const data = await db.Workout.find({})
       .sort({ _id: -1 })
       .limit(1);
+    // Here we calculate the total duration of all exercises in this workout.
     const duration = data[0].exercises.reduce((sum, current) => {
       return sum + current.duration;
     }, 0);
-    console.log(data[0]);
-    res.json(data);
+    // Here we create a new array with the total duration key/value pair.
+    const exercise = [
+      {
+        _id: data[0]._id,
+        day: data[0].day,
+        totalDuration: duration,
+        exercises: data[0].exercises
+      }
+    ]
+    res.json(exercise);
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
   }
-})
+});
 
 // This route creates an instance of Workout.
 router.post("/workouts", async (req, res) => {
   try {
     const data = await db.Workout.create(req);
-    console.log(data);
     res.json(data);
   } catch (err) {
     console.log(err);
@@ -36,13 +45,15 @@ router.put("/workouts/:id", async ({ body, params }, res) => {
     const data = await db.Workout.updateOne({
       "_id": params.id,
     }, {
-      $addToSet: {"exercises": [ body ]}
+      $addToSet: { "exercises": [body] }
     })
     res.json(data);
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
   }
-})
+});
+
+// This route will get 
 
 module.exports = router;
