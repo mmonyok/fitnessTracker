c7ec78b2be188de539259ab4276b4c4ff747331c
Ody <melodymonyok@gmail.com>
2021-08-23 22:31:18 -0500

    Added the route to add an exercise to a workout collection.

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -7,7 +7,10 @@ router.get("/workouts", async (req, res) => {
     const data = await db.Workout.find({})
       .sort({ _id: -1 })
       .limit(1);
-    console.log(data.day);
+    const duration = data[0].exercises.reduce((sum, current) => {
+      return sum + current.duration;
+    }, 0);
+    console.log(data[0]);
     res.json(data);
   } catch (err) {
     console.log(err);
@@ -26,5 +29,20 @@ router.post("/workouts", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+// This route will pull the data from the last workout.
+router.put("/workouts/:id", async ({ body, params }, res) => {
+  try {
+    const data = await db.Workout.updateOne({
+      "_id": params.id,
+    }, {
+      $addToSet: {"exercises": [ body ]}
+    })
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+})
 
 module.exports = router;
